9c58162b982c700e74d1b9bc0205b2851a03aeb2
Peter Ngo <ngo@semafor.ch>
2025-06-21 22:18:51 +0200

    chore: remove comments

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'http://localhost:8080/realms/GeoCalc3D',
-  redirectUri: window.location.origin + '/login', // WICHTIG!
+  redirectUri: window.location.origin + '/login',
   clientId: 'geocalc-backend',
   responseType: 'code',
   scope: 'openid profile email',
